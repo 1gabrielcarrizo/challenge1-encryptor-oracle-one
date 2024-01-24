fc73eb6bcfcf7852b1904cc9329eace51157a22c
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-24 19:03:57 -0300

    testing...

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 import { changeTheme } from "./helpers/changeTheme.js";
 
 const toggleTheme = document.getElementById("toggle-theme");
+const footerText = document.getElementById("footerText");
+const date = new Date().getFullYear();
+
+footerText.innerHTML = `Copyright &copy; ${date} all rights reserved by <a href="https://www.linkedin.com/in/1gabrielcarrizo/" target="_blank">Gabriel Carrizo</a>`
 
 toggleTheme.addEventListener("click", changeTheme);

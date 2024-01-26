00cc7a36209e38a2f5f06d4e55e3d9add9549e40
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2024-01-26 11:59:48 -0300

    finishing the function to decrypt

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ const toggleTheme = document.getElementById("toggle-theme"),
       textoOculto = document.getElementById("textoOculto"),
       sinTextoContainer = document.getElementById("sinTexto-container"),
       conTextoContainer = document.getElementById("conTexto-container"),
+      textoOcultoContainer = document.getElementById("textoOcultoContainer"),
       date = new Date().getFullYear(),
       equivalenciaCifrada = {
         'a': 'ai',
@@ -28,24 +29,28 @@ const toggleTheme = document.getElementById("toggle-theme"),
     };
 let palabrasEnElTextarea = "",
     textoCifrado = "",
-    textoOriginal = "";
+    // equivalenciaActual = "",
+    textoDecifrado = "";
 
 
 footerText.innerHTML = `Copyright &copy; ${date}. All rights are reserved`
 
 const encriptarTexto = () => {
     palabrasEnElTextarea = textarea.value;
-    console.log(palabrasEnElTextarea);
+    console.log(`texto original: ${palabrasEnElTextarea}`);
 
     for (let i = 0; i < palabrasEnElTextarea.length; i++) {
         const letraActual = palabrasEnElTextarea.charAt(i);
         textoCifrado += equivalenciaCifrada[letraActual] || letraActual;
     }
-    console.log(textoCifrado)
+    console.log(`texto cifrado: ${textoCifrado}`)
+    deshabilitarBoton(btnEncriptar);
+    btnEncriptar.classList.add("boton-deshabilitado");
+    textarea.setAttribute("readonly", true)
 }
 
 const mostrarTextoCifrado = () => {
-    textoOculto.innerHTML = textoCifrado;
+    textoOculto.textContent = textoCifrado;
     textarea.value = "";
     sinTextoContainer.classList.add("ocultar");
     conTextoContainer.classList.remove("ocultar");
@@ -54,17 +59,35 @@ const mostrarTextoCifrado = () => {
 
 const desencriptarTexto = () => {
     palabrasEnElTextarea = textarea.value;
-    console.log(palabrasEnElTextarea);
+    console.log("se hizo click en el boton desencriptar")
+    console.log(`texto cifrado: ${palabrasEnElTextarea}`);
 
-    for (let i = 0; i < palabrasEnElTextarea.length; i++) {
-        const letraActual = palabrasEnElTextarea.charAt(i);
-        textoOriginal += equivalenciaOriginal[letraActual] || letraActual;
+    let i = 0;
+
+    while (i < palabrasEnElTextarea.length) {
+        let equivalenciaActual = "";
+
+        for (let j = i; j < palabrasEnElTextarea.length; j++) {
+            equivalenciaActual += palabrasEnElTextarea[j];
+
+            if (equivalenciaOriginal[equivalenciaActual]) {
+                textoDecifrado += equivalenciaOriginal[equivalenciaActual];
+                i = j + 1;
+                break;
+            } else if (j === palabrasEnElTextarea.length - 1) {
+                textoDecifrado += palabrasEnElTextarea[i];
+                i++;
+            }
+        }
     }
-    console.log(textoOriginal)
+
+    deshabilitarBoton(btnDesencriptar);
+    btnDesencriptar.classList.add("boton-deshabilitado");
+    console.log(`texto decifrado: ${textoDecifrado}`);
 }
 
 const habilitarDeshabilitarBtnEncriptar = () => {
-    (indicarSiContieneCaracteresEspeciales(textarea.value)) ? 
+    (indicarSiContieneCaracteresEspeciales(textarea.value) || textarea.value.length < 1) ? 
         (deshabilitarBoton(btnEncriptar),
         btnEncriptar.classList.add("boton-deshabilitado"))
     :
@@ -72,10 +95,24 @@ const habilitarDeshabilitarBtnEncriptar = () => {
         btnEncriptar.classList.remove("boton-deshabilitado"))
 }
 
+const copiarTextoCifrado = () => {
+    alert("Se ha copiado con exito el contenido y se lo ha pegado automaticamente en el textarea para descifrarlo")
+    // console.log("se hizo click en el boton copiar");
+    // console.log(textoOculto.textContent)
+    sinTextoContainer.classList.remove("ocultar");
+    conTextoContainer.classList.add("ocultar");
+    textoOculto.classList.add("ocultar");
+    btnCopiar.classList.add("ocultar");
+    textoOcultoContainer.classList.add("ocultar");
+    textarea.value = textoOculto.textContent; //textarea
+    habilitarBoton(btnDesencriptar);
+    btnDesencriptar.classList.remove("boton-deshabilitado");
+    console.clear()
+}
 
 toggleTheme.addEventListener("click", changeTheme); // modo claro y oscuro
 textarea.addEventListener("input", habilitarDeshabilitarBtnEncriptar); // habilitar o deshabilitar btn
 btnEncriptar.addEventListener("click", encriptarTexto);
 btnEncriptar.addEventListener("click", mostrarTextoCifrado);
-// btnEncriptar.addEventListener("click", desencriptarTexto);
-// btnDesencriptar.addEventListener("click", desencriptarTexto);
+btnCopiar.addEventListener("click", copiarTextoCifrado);
+btnDesencriptar.addEventListener("click", desencriptarTexto);
